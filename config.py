3f066db01cf58002d15fc495aaecafd7851955f3
ocim-laravel <142159206+ocim-laravel@users.noreply.github.com>
2025-03-19 19:31:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "DB_catat"
+import os
+
+DB_HOST = os.getenv("MYSQLHOST", "localhost")
+DB_USER = os.getenv("MYSQLUSER", "root")
+DB_PASSWORD = os.getenv("MYSQLPASSWORD", "")
+DB_NAME = os.getenv("MYSQLDATABASE", "railway")
+DB_PORT = int(os.getenv("MYSQLPORT", 3306))
